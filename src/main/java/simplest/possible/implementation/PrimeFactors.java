c6453cfc887a6d570011a429d62e1d54110c5e27
manishakumari98 <mk98.manisha@gmail.com>
2021-01-04 13:49:14 +0530

    Initial Commit

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,10 @@
+package simplest.possible.implementation;
+
+import java.util.*;
+
+public class PrimeFactors {
+
+    public List<Integer> of(int number) {
+        return new ArrayList<>();
+    }
+}
